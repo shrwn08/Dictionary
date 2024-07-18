35355d14409abe532191e478781eda9df6711f67
shrwn08 <shrwnkumar08@gmail.com>
2024-07-19 03:48:58 +0530

    second commit

--- x-dictionary/src/components/Dictionary.js
+++ x-dictionary/src/components/Dictionary.js
@@ -4,42 +4,38 @@ const Dictionary = () => {
   const [input, setInput] = useState("");
   const [response, setResponse] = useState("");
 
-  const handleSubmitBtn = (event) => {
-    event.preventDefault();
-    setInput(event.target.value);
-    // console.log(event.target.value);
-    let matchedWord =[]
-    if(input === undefined){
-        setResponse('')
-    }else{
-
-     matchedWord= dictionaryData.find((item)=>item.word.toLowerCase() === input.toLowerCase())
+  const handleSubmitBtn = () => {
+    
+    let matchedWord = [];
+    if (input === undefined) {
+      setResponse("");
+    } else {
+      matchedWord = dictionaryData.find(
+        (item) => item.word.toLowerCase() === input.toLowerCase()
+      );
     }
-    console.log('matched',matchedWord)
-    if(!matchedWord){
-        setResponse('Word not found in the dictionary')
-    }else{
-        setResponse(matchedWord.meaning)
-        setInput('')
+    console.log("matched", matchedWord);
+    if (!matchedWord) {
+      setResponse("Word not found in the dictionary");
+    } else {
+      setResponse(matchedWord.meaning);
+      setInput("");
     }
   };
 
- 
   return (
-    <div>
-      <form onSubmit={handleSubmitBtn}>
-        <h1>Dictionary App</h1>
-        <input
-          value={input}
-          onChange={handleSubmitBtn}
-          placeholder="Search for a word"
-          required
-        />
-        <button type="submit">Search</button>
-        <h3>Dictionary: </h3>
-        <p>{response}</p>
-      </form>
-    </div>
+    <>
+      <h1>Dictionary App</h1>
+      <input
+        value={input}
+        onChange={(event)=>setInput(event.target.value)}
+        placeholder="Search for a word"
+        required
+      />
+      <button onClick={handleSubmitBtn}>Search</button>
+      <h3>Dictionary: </h3>
+      <p>{response}</p>
+    </>
   );
 };
 
